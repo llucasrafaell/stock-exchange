8fc389e954640179097adc3f70f0a5f1501057a5
Lucas <lucasmarthins1@gmail.com>
2021-06-11 18:45:18 -0300

    Update commit StockExchange.py

--- StockExchange.py
+++ StockExchange.py
@@ -1,37 +1,32 @@
 import requests
 import time
-
+import json
 while True:
-
-    #Importa as cotações pela API
-    cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-    cotacoes = cotacoes.json()
-
-#Filtra os valores desejados em Dólar
-    cotacaoDolar = cotacoes['USDBRL']['bid']
-    valorMaxDolar = cotacoes['USDBRL']['high']
-    valorMinDolar = cotacoes['USDBRL']['low']
+    #Import quotes via API
+    quotation = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+    quotation = quotation.json()
+    
+    #Filter the desired values in Dollars
+    quotation_dolar = quotation['USDBRL']['bid']
     value_pay_dolar = '5.12'
-
-# Filtra os valores desejados em Euro
-    cotacaoEuro = cotacoes['EURBRL']['bid']
-    valorMaxEuro = cotacoes['EURBRL']['high']
-    valorMinEuro = cotacoes['EURBRL']['low']
+    #Filters the desired values in Euro
+    quotation_euro = quotation['EURBRL']['bid']
     value_pay_euro ='6.196'
-# Filtra os valores desejados em Bitcoin
-    cotacaoBitcoin = cotacoes['BTCBRL']['bid']
-    valorMaxBitcoin = cotacoes['BTCBRL']['high']
-    valorMinBitcoin = cotacoes['BTCBRL']['low']
-    value_pay_bitcoin = '190000'
-    print('Dólar $:', cotacaoDolar,'R$ |', 'Euro €:',cotacaoEuro,'R$ |','BitCoin ₿:',valorMaxBitcoin,'R$')
+    #Filter the desired values in Bitcoin
+    quotation_bitcoin = quotation['BTCBRL']['bid']
+    value_pay_bitcoin = '190000' \
+                        ''
+    #Print desired values
+    print('Dólar $:', quotation_dolar,'R$ |', 'Euro €:',quotation_euro,'R$ |','BitCoin ₿:',quotation_bitcoin,'R$')
 
-    if (cotacaoDolar < value_pay_dolar):
+    #Compare the values with the objective
+    if (quotation_dolar < value_pay_dolar):
         print ('O dólar chegou no valor desejado')
-    if (cotacaoEuro < value_pay_euro):
+    if (quotation_euro < value_pay_euro):
         print('O Euro chegou no valor desejado')
-    if (cotacaoEuro < value_pay_euro):
+    if (quotation_bitcoin < value_pay_euro):
         print('A bitcoin chegou no valor desejado')
-        
+
     time.sleep(2)
 
 
